fbc264be31e92e46a26eaf15c03718f202325b62
Kenanshakra <89753703+Kenanshakra@users.noreply.github.com>
2023-01-25 13:56:39 +0100

    .

---
+++ src/main/java/com/example/nettbank/NettbankApplication.java
@@ -0,0 +1,13 @@
+package com.example.nettbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettbankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettbankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nettbank/NettbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nettbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettbankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
