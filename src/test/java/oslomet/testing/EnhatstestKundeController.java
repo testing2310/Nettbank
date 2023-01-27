f1cde69110f9ae7d5115281ef4cd0d6e452bc88b
Kenanshakra <89753703+Kenanshakra@users.noreply.github.com>
2023-01-27 14:27:31 +0100

    AdminKundeContrller fikset

--- src/test/java/oslomet/testing/EnhatstestKundeController.java
+++ src/test/java/oslomet/testing/EnhatstestKundeController.java
@@ -1,16 +1,20 @@
 package oslomet.testing;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 import oslomet.testing.API.AdminKundeController;
 import oslomet.testing.DAL.AdminRepository;
 import oslomet.testing.Models.Kunde;
+import oslomet.testing.Sikkerhet.Sikkerhet;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 
+@RunWith(MockitoJUnitRunner.class)
 public class EnhatstestKundeController {
 
     @InjectMocks
@@ -21,40 +25,45 @@ public class EnhatstestKundeController {
     // denne skal Mock'es
     private AdminRepository repository;
 
+    @Mock
+    private Sikkerhet sjekk;
+
     @Test
-    public void test_registrereKundeOK() {
+    public void test_registrerKundeOK() {
 
         // arrage
         Kunde kunde1 = new Kunde("01010110523",
                 "Lene", "Jensen", "Askerveien 22", "3270",
                 "Asker", "22224444", "HeiHei");
 
+        Mockito.when(sjekk.loggetInn()).thenReturn("Admin");
+        Mockito.when(repository.registrerKunde(kunde1)).thenReturn("OK");
 
-        Mockito.when(repository.registrerKunde((any(Kunde.class)))).thenReturn("OK");
 
         // act
-        String resultat = kundeController.lagreKunde(kunde1); // husk å bruke denne Controlleren, ikke opprett en ny!
+        String resultat = kundeController.lagreKunde(kunde1); //
 
         // assert
         assertEquals("OK", resultat);
+
     }
 
     @Test
-    public void test_registrereKundeFeil() {
+    public void test_registrerKundeFeil() {
 
         // arrage
         Kunde kunde1 = new Kunde("01010110523",
                 "Lene", "Jensen", "Askerveien 22", "3270",
                 "Asker", "22224444", "HeiHei");
 
-
-        Mockito.when(repository.registrerKunde((any(Kunde.class)))).thenReturn("Feil");
+        Mockito.when(sjekk.loggetInn()).thenReturn("Admin");
+        Mockito.when(repository.registrerKunde(kunde1)).thenReturn("Feil");
 
         // act
-        String resultat = kundeController.lagreKunde(kunde1); // husk å bruke denne Controlleren, ikke opprett en ny!
 
+        String resultat = kundeController.lagreKunde(kunde1); //
         // assert
-        assertEquals("OK", resultat);
+        assertEquals("Feil", resultat);
     }
 
 }
